57b4a6f2356a866d391817164e73bcdb81f3f767
dinithchanukakulasinghe <dinithcdev@gmail.com>
2025-09-02 20:15:02 +0530

    Initial commit

---
+++ lib/routing/app_router.dart
@@ -0,0 +1,32 @@
+import 'package:go_router/go_router.dart';
+
+import '../presentation/screens/favorites_screen.dart';
+import '../presentation/screens/job_details_screen.dart';
+import '../presentation/screens/jobs_screen.dart';
+import '../presentation/screens/splash_screen.dart';
+
+final GoRouter router = GoRouter(
+  initialLocation: '/splash',
+  routes: [
+    GoRoute(
+      path: '/splash',
+      name: 'splash',
+      builder: (context, state) => const SplashScreen(),
+    ),
+    GoRoute(
+      path: '/jobs',
+      name: 'jobs',
+      builder: (context, state) => const JobsScreen(),
+    ),
+    GoRoute(
+      path: '/job-details',
+      name: 'job-details',
+      builder: (context, state) => const JobDetailsScreen(),
+    ),
+    GoRoute(
+      path: '/favorites',
+      name: 'favorites',
+      builder: (context, state) => const FavoritesScreen(),
+    ),
+  ],
+);
