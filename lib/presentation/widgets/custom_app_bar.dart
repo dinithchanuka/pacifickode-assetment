d6c619e878d15b58816745cf26739abda74336b2
dinithchanukakulasinghe <dinithcdev@gmail.com>
2025-09-04 19:31:18 +0530

    Process : Change theme

--- lib/presentation/widgets/custom_app_bar.dart
+++ lib/presentation/widgets/custom_app_bar.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+import '../../core/providers/theme_provider.dart';
+
+class CustomAppBar extends ConsumerWidget implements PreferredSizeWidget {
   final String title;
 
   const CustomAppBar({super.key, required this.title});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context,WidgetRef ref) {
+
+    final theme = ref.watch(themeModeProvider);
+    final notifier = ref.read(themeModeProvider.notifier);
+
     return AppBar(
       title: Text(title),
       actions: [
         IconButton(
           icon: Icon(Icons.dark_mode_rounded),
           onPressed: () {
-            // Handle search action
+            notifier.state = theme == ThemeMode.dark
+                ? ThemeMode.light
+                : ThemeMode.dark;
           },
         ),
       ],
